56b292e37d5a3945d9afc8e0ea471f60c0f5f9bd
cecerealll <cecilian.fan123@gmail.com>
2022-07-22 18:50:39 -0400

    add built

--- src/App.js
+++ src/App.js
@@ -1,67 +1,67 @@
 import { useState, useEffect } from "react";
-import Users from "./Users";
 import { fetchUsers, fetchTodos } from "./api";
+import Users from "./Users";
 import "./App.css";
 
 function App() {
-  const [users, setUsers] = useState([]);
-  const [todos, setTodos] = useState([]);
-  const [searchInput, setSearchInput] = useState("");
-  const [domainFilter, setDomainFilter] = useState("");
+    const [users, setUsers] = useState([]);
+    const [todos, setTodos] = useState([]);
+    const [searchInput, setSearchInput] = useState("");
+    const [domainFilter, setDomainFilter] = useState("");
 
-  const handleChange = (e) => {
-    setSearchInput(e.target.value);
-    // users.filter((user) => user.nameincludes(searchInput));
-  };
+    const handleChange = (e) => {
+        setSearchInput(e.target.value);
+    };
 
-  const handleSelect = (e) => {
-    setDomainFilter(e.target.value);
-    // users.filter((user) => user.nameincludes(searchInput));
-  };
+    const handleSelect = (e) => {
+        setDomainFilter(e.target.value);
+    };
 
-  const handleCheck = (id) => {
-    const newTodos = todos.map((todo) => {
-      if (todo.id === id) {
-        todo.completed = !todo.completed;
-      }
-      return todo;
-    });
-    setTodos(newTodos);
-  };
+    const handleCheck = (id) => {
+        const newTodos = todos.map((todo) => {
+            if (todo.id === id) {
+                todo.completed = !todo.completed;
+            }
+            return todo;
+        });
+        setTodos(newTodos);
+    };
 
-  const handleDelete = (id) => {
-    const newTodos = todos.filter((todo) => todo.id !== id);
-    setTodos(newTodos);
-  };
+    const handleDelete = (id) => {
+        const newTodos = todos.filter((todo) => todo.id !== id);
+        setTodos(newTodos);
+    };
 
-  useEffect(() => {
-    (async () => {
-      setUsers(await fetchUsers());
-      setTodos(await fetchTodos());
-    })();
-    console.log("useEffect");
-  }, []);
+    useEffect(() => {
+        (async () => {
+            setUsers(await fetchUsers());
+            setTodos(await fetchTodos());
+        })();
+    }, []);
 
-  return (
-    <div className='App'>
-      <input type='text' onChange={handleChange} />
-      <select name='' id='' defaultValue onChange={handleSelect}>
-        <option value></option>
-        <option value='com'>.com</option>
-        <option value='net'>.net</option>
-        <option value='other'>other</option>
-      </select>
-      {/* <button >search</button> */}
-      <Users
-        users={users}
-        searchInput={searchInput}
-        domainFilter={domainFilter}
-        handleCheck={handleCheck}
-        handleDelete={handleDelete}
-        todos={todos}
-      />
-    </div>
-  );
+    return (
+        <div className='App'>
+            <input
+                type='text'
+                onChange={handleChange}
+                className={"searchInput"}
+            />
+            <select defaultValue onChange={handleSelect}>
+                <option value></option>
+                <option value='com'>.com</option>
+                <option value='net'>.net</option>
+                <option value='other'>other</option>
+            </select>
+            <Users
+                users={users}
+                searchInput={searchInput}
+                domainFilter={domainFilter}
+                handleCheck={handleCheck}
+                handleDelete={handleDelete}
+                todos={todos}
+            />
+        </div>
+    );
 }
 
 export default App;
--- src/api.js
+++ src/api.js
@@ -1,11 +1,11 @@
 export const fetchUsers = () => {
-  return fetch("https://jsonplaceholder.typicode.com/users").then((resp) =>
-    resp.json()
-  );
+    return fetch("https://jsonplaceholder.typicode.com/users").then((resp) =>
+        resp.json()
+    );
 };
 
 export const fetchTodos = () => {
-  return fetch("https://jsonplaceholder.typicode.com/todos").then((resp) =>
-    resp.json()
-  );
+    return fetch("https://jsonplaceholder.typicode.com/todos").then((resp) =>
+        resp.json()
+    );
 };
